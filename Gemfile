28dd561b6419205499f1400a76b77a545c03c9c7
Wesley Vance <wesley.vance@mac.com>
2015-03-30 22:44:17 -0500

    Added Gravatar Gem

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ gem 'tzinfo-data'
 #FOR USER ACCOUNTS 
 gem 'devise'
 
+# ADDED GRAVATAR 
+gem 'gravtastic'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
 # Use sqlite3 as the database for Active Record
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  # USED FOR PROFILE PICTURE: LIBRARY
+  include Gravtastic
+	gravtastic
 end
