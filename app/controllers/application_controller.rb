207fcab68ab97193f2aa1ff2700d8d3f79d2d54a
Wesley Vance <wesley.vance@mac.com>
2015-04-01 00:15:59 -0500

    Fixed Format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  #FORCES USER TO SIGN UP BEFORE GOING TO SITE
-  before_action :authenticate_user!
-
-  def index
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  #FORCES USER TO SIGN UP BEFORE GOING TO SITE
+ def is_active?(link_path)
+  current_page?(link_path) ? "active" : ""
+ end
 end
