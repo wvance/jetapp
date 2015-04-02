e43a0a0dcdc30d5f3a660413a5f7e4fac2bb92c0
vinigre <cak2362@tamu.edu>
2015-04-02 15:54:41 -0500

    Associated comments with the users that made them.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
 	belongs_to :idea
+	belongs_to :user
+	validates :user_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150401191447) do
+ActiveRecord::Schema.define(version: 20150402194435) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "idea_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "ideas", force: :cascade do |t|
