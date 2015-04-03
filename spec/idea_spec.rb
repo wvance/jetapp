63f014419c73a8f7a6769bb7c855fd436b30321a
Wesley Vance <wesley.vance@mac.com>
2015-04-02 19:12:55 -0500

    Added idea style

---
+++ spec/idea_spec.rb
@@ -0,0 +1,9 @@
+require "spec-helper"
+require "idea"
+
+describe Idea do
+  it "has a title" do # yep, you can totally use 'it'
+    idea = Idea.new # creating a new idea 'instance'
+    idea.title.should be_true # this is our expectation
+  end
+end
