dea9e52dc64936ed15e7b4b22e8beb7923ae6dd6
Wesley Vance <wesley.vance@mac.com>
2015-04-05 18:10:09 -0500

    Attempt fix 1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap-material-design
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-material-design
 //= require turbolinks
 //= require_tree .
