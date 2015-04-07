f9fe1778994c0f26c0d4d9c9c9d956abb7bb8384
Wesley Vance <wesley.vance@mac.com>
2015-04-07 11:19:51 -0500

    Added Random Depth & Shuffle

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,4 +1,7 @@
 $(function () {
-	e.preventDefault()
-  $('#myTab a:first').tab('show')
-})
+  var depthClass = ["z-depth-1", "z-depth-2", "z-depth-3", "z-depth-4", "z-depth-5"];
+
+    $(".randomDepth").each(function(){
+        $(this).addClass(depthClass[~~(Math.random()*depthClass.length)]);
+    });
+});
