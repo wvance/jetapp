fe28f864e0e2b6b873d0844de30b5221f93f0f2c
Wesley Vance <wesley.vance@mac.com>
2015-04-11 19:58:08 -0500

    ShowAll Edits

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150409172047) do
+ActiveRecord::Schema.define(version: 20150402194435) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -48,9 +48,9 @@ ActiveRecord::Schema.define(version: 20150409172047) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "avatar"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
