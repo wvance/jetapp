fa629d94df5abbcbfa9bab553399ece052638c6a
Wesley Vance <wesley.vance@mac.com>
2015-04-13 00:13:20 -0500

    Added Side NavBar

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,7 +1,7 @@
 $(function () {
   var depthClass = ["z-depth-1", "z-depth-2", "z-depth-3", "z-depth-4", "z-depth-5"];
 
-    $(".randomDepth").each(function(){
-        $(this).addClass(depthClass[~~(Math.random()*depthClass.length)]);
-    });
+  $(".randomDepth").each(function(){
+      $(this).addClass(depthClass[~~(Math.random()*depthClass.length)]);
+  });
 });
