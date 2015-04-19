d569ad71dea5e057cbdcb5eb69328ecdc831c67e
Wesley Vance <wesley.vance@mac.com>
2015-04-19 18:24:17 -0500

    changed where to find

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -6,6 +6,6 @@ class ActivitiesController < ApplicationController
   	# friend_ids = current_user.friends.map(&:id)
   	# @activities = Activity.where("user_id in (?)", friend_ids.push(current_user.id)).order("created_at desc").all
 
-  	@activities = Activity.where(current_user.id).all
+  	@activities = Activity.find(current_user.id).all
   end
 end
