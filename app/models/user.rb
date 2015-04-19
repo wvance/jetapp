e25cc7792a5eb6d62448abeda55d7697fc36077e
Wesley Vance <wesley.vance@mac.com>
2015-04-19 18:44:22 -0500

    Attempt 2 fix login

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,7 +45,7 @@ class User < ActiveRecord::Base
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions.to_hash).where(["lower(login) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      where(conditions.to_hash).where(["lower(profileName) = :value OR lower(email) = :value", { :value => login.downcase }]).first
     else
       where(conditions.to_hash).first
     end
