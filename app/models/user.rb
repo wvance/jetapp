aeebcf69d484706705405976c4a6917ab2158b07
Wesley Vance <wesley.vance@mac.com>
2015-04-19 19:34:40 -0500

    username:

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,7 @@ class User < ActiveRecord::Base
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions.to_hash).where(["lower(profileName) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
     else
       where(conditions.to_hash).first
     end
