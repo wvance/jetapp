7e05d958b31bc4e4896162c153ce01215d5ff01a
Wesley Vance <wesley.vance@mac.com>
2015-04-19 19:27:53 -0500

    Final attempt

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :authentication_keys => [:login]
+         :recoverable, :rememberable, :trackable, :validatable
 
  	mount_uploader :avatar, AvatarUploader
 
@@ -35,18 +34,21 @@ class User < ActiveRecord::Base
   # This is in addition to a real persisted field like 'profileName'
   # https://github.com/plataformatec/devise/wiki/How-To:-Allow-users-to-sign-in-using-their-username-or-email-address
   attr_accessor :login
-  def login=(login)
-    @login = login
-  end
-
-  def login
-    @login || self.profileName || self.email
-  end
+  
 
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
       where(conditions.to_hash).where(["profileName = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions.to_hash).first
+    end
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(profileName) = :value OR lower(email) = :value", { :value => login.downcase }]).first
     else
       where(conditions.to_hash).first
     end
