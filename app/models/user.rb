f1e420950a4f287e2ce224b083c562d51b6a0335
Wesley Vance <wesley.vance@mac.com>
2015-04-19 19:37:14 -0500

    Require Email

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,10 +35,11 @@ class User < ActiveRecord::Base
   # https://github.com/plataformatec/devise/wiki/How-To:-Allow-users-to-sign-in-using-their-username-or-email-address
   attr_accessor :login
   
+  # FIX THIS LATER
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      where(conditions.to_hash).where(["lower(email) = :value", { :value => login.downcase }]).first
     else
       where(conditions.to_hash).first
     end
