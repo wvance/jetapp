17150e689b146cefef48bba76edf54945e79bffd
vinigre <cak2362@tamu.edu>
2015-04-22 09:53:20 -0500

    added parent id to comments.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150419153101) do
+ActiveRecord::Schema.define(version: 20150422145235) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "user_id"
@@ -28,9 +28,10 @@ ActiveRecord::Schema.define(version: 20150419153101) do
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "idea_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.integer  "user_id"
+    t.integer  "parent_comment_id"
   end
 
   create_table "ideas", force: :cascade do |t|
