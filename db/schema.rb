0550cfa63ec26a0e8f0589489fbe6d2d8a44f403
Wesley Vance <wesley.vance@mac.com>
2015-04-22 13:36:04 -0500

    fixed Schema Merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420203133) do
+ActiveRecord::Schema.define(version: 20150422145235) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "user_id"
