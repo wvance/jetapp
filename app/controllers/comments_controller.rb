1a5d7ecca76f29f04df39f53b1b96303f24f8f9b
vinigre <cak2362@tamu.edu>
2015-04-26 20:31:01 -0500

    Can now submit replies, but data is not recorded.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,6 @@ class CommentsController < ApplicationController
   # GET /comments/new
   def new
 		@comment = Comment.new
-		@pcid = params[:data]["parent_comment_id"]
 	end
 
   # # GET /comments/1/edit
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,5 +5,5 @@ class Comment < ActiveRecord::Base
 	belongs_to :parent, :class_name => "Comment", :foreign_key => "parent_comment_id"
 	has_many :child_comments, :class_name => "Comment", :foreign_key => "parent_comment_id"
 
-	validates :body, :user_id, :idea_id, presence: true
+	validates :body, :user_id, :idea_id, :presence: true
 end
