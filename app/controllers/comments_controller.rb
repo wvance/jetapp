7554fa570dfd4fd33e127c56014258753ebe27d1
vinigre <cak2362@tamu.edu>
2015-04-26 20:42:41 -0500

    Yeah, works better now, it's trusted param.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -73,6 +73,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:body, :idea_id, :user_id)
+      params.require(:comment).permit(:body, :idea_id, :user_id, :parent_comment_id)
     end
 end
