f1bb496fb95c0503b8fbc70f7ffc64effabbd3c2
Wesley Vance <wesley.vance@mac.com>
2015-04-30 21:47:33 -0500

    Added Card style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,6 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  
 protected
  
   def configure_permitted_parameters
@@ -17,5 +16,19 @@ protected
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :profileName, :email, :password, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:firstName, :lastName, :profileName, :email, :password,
     :password_confirmation, :current_password, :avatar, :avatar_cache) }
+  end
+end
+
+def flash_message
+  if flash[:notice]
+    %{#{flash[:notice]}}
+  elsif flash[:warning]
+    %{#{flash[:warning]}}
+  elsif flash[:note]
+    %{#{flash[:note]}}
+  elsif flash[:alert]
+    %{#{flash[:alert]}}
+  elsif flash[:error]
+    %{#{flash[:error]}}
   end
 end
