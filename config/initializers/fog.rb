58318f78f4ef0ff2112c1b88f4883170b5fa4530
Wesley Vance <wesley.vance@mac.com>
2015-05-01 16:14:52 -0500

    Commented out aws storage

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,9 +1,11 @@
-CarrierWave.configure do |config|
-	config.fog_credentials = {
-		:provider => 'AWS', # required
-		:aws_access_key_id => ENV["AWS_ID"], # required
-		:aws_secret_access_key => ENV["AWS_SECRET"], # required
-	}
-	config.fog_directory = 'jetpackapp/images' # required
-end
+# UNCOMMENT FOR AWS FILE STORAGE
+
+# CarrierWave.configure do |config|
+# 	config.fog_credentials = {
+# 		:provider => 'AWS', # required
+# 		:aws_access_key_id => ENV["AWS_ID"], # required
+# 		:aws_secret_access_key => ENV["AWS_SECRET"], # required
+# 	}
+# 	config.fog_directory = 'jetpackapp/images' # required
+# end
 
