7b431f59a8f41126bde9ee6f3c62069592b8870d
Wesley Vance <wesley.vance@mac.com>
2015-05-02 14:57:06 -0500

    Fixed User Profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
   def showProfile
   	@user_byName = User.find_by_profileName(params[:id]) 						# RETURNS USER OBJECT: NOTE NOT THE ID
   	@user_comments = Comment.where(:user_id => @user_byName.id)
-  	@user_ideas = Idea.where(:author => @user_byName.profileName).page(params[:page]).per(8)
+  	@user_ideas = Idea.where(:author => @user_byName.profileName).page(params[:page]).per(6)
   	@user_friendships = @user_byName.user_friendships.all
   	@friendship = current_user.user_friendships.where(:friend_id => @user_byName.id).pluck(:id) # RETURNS AN ID
   end
