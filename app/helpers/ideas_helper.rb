f1c68f1fc98c99c0629ab246b468101a5536a9e9
Wesley Vance <wesley.vance@mac.com>
2015-05-02 23:10:10 -0500

    Chnaged Format

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -1,21 +1,22 @@
 module IdeasHelper
 
 	def ideaCardColor(ideaSector)
-    sectorColors = Hash[[	['Defense & Security','red lighten-2'],
-											    ['Infrastructure','orange lighten-2'],
-											    ['Communications','lime-green lighten-2'],
-											    ['Energy/Utilities','yellow lighten-2'],
-											    ['Chemicals','purple lighten-2'],
-											    ['Construction','brown lighten-2'],
-											    ['Entertainment','light-blue lighten-2'],
-											    ['Financial','green lighten-2'],
-											    ['Government','teal lighten-2'],
-											    ['Healthcare','pink lighten-2'],
-											    ['Manufacturing','deep-orange lighten-2'],
-											    ['Retail','indigo lighten-2'],
-											    ['Technology','blue lighten-2'],
-											    ['Other', 'blue-grey lighten-2']
-    										]]
+    sectorColors = Hash[[	
+    	['Defense & Security','red lighten-2'],
+	    ['Infrastructure','orange lighten-2'],
+	    ['Communications','lime-green lighten-2'],
+	    ['Energy/Utilities','yellow lighten-2'],
+	    ['Chemicals','purple lighten-2'],
+	    ['Construction','brown lighten-2'],
+	    ['Entertainment','light-blue lighten-2'],
+	    ['Financial','green lighten-2'],
+	    ['Government','teal lighten-2'],
+	    ['Healthcare','pink lighten-2'],
+	    ['Manufacturing','deep-orange lighten-2'],
+	    ['Retail','indigo lighten-2'],
+	    ['Technology','blue lighten-2'],
+	    ['Other', 'blue-grey lighten-2']
+		]]
     if (sectorColors.has_key?(ideaSector))
       sectorColors[ideaSector]
     else 
