64af6e6515fb0f197fe45e3f4f7b9250054dc12e
Wesley Vance <wesley.vance@mac.com>
2015-05-02 23:07:07 -0500

    Added Sectors and full colors

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -1,7 +1,21 @@
 module IdeasHelper
 
 	def ideaCardColor(ideaSector)
-    sectorColors = Hash[ [ ["Technology", "red lighten-3"], ["Energy", "yellow lighten-3"] ] ]
+    sectorColors = Hash[[	['Defense & Security','red lighten-2'],
+											    ['Infrastructure','orange lighten-2'],
+											    ['Communications','lime-green lighten-2'],
+											    ['Energy/Utilities','yellow lighten-2'],
+											    ['Chemicals','purple lighten-2'],
+											    ['Construction','brown lighten-2'],
+											    ['Entertainment','light-blue lighten-2'],
+											    ['Financial','green lighten-2'],
+											    ['Government','teal lighten-2'],
+											    ['Healthcare','pink lighten-2'],
+											    ['Manufacturing','deep-orange lighten-2'],
+											    ['Retail','indigo lighten-2'],
+											    ['Technology','blue lighten-2'],
+											    ['Other', 'blue-grey lighten-2']
+    										]]
     if (sectorColors.has_key?(ideaSector))
       sectorColors[ideaSector]
     else 
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -3,9 +3,20 @@ class Idea < ActiveRecord::Base
 	has_many :stickies
 	# has_many :users
 	SECTORS = [
+    ['Defense & Security','Defense & Security'],
+    ['Infrastructure','Infrastructure'],
+    ['Communications','Communications'],
+    ['Energy/Utilities','Energy/Utilities'],
+    ['Chemicals','Chemicals'],
+    ['Construction','Construction'],
+    ['Entertainment','Entertainment'],
+    ['Financial','Financial'],
+    ['Government','Government'],
+    ['Healthcare','Healthcare'],
+    ['Manufacturing','Manufacturing'],
+    ['Retail','Retail'],
     ['Technology','Technology'],
-    ['Energy','Energy'],
-    ['Bio-Tech','Bio-Tech']
+    ['Other', 'Other']
   ]
 
 	belongs_to :user
